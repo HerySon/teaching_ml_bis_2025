5d6563c46e4e2afd087464103bef3e40d9196609
FeurKing <39668417+feur25@users.noreply.github.com>
2025-02-16 18:12:18 +0100

    feat(dataframe): add imputation and filtering of missing data

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,6 +2,7 @@ import argparse
 import asyncio
 
 from .data_frame_processor import DataFrameProcessor
+from .data_frame_filter import DataFrameSelector
 
 __all__ = ['DataFrameProcessor']
 
@@ -32,17 +33,47 @@ def pandas_display() -> str:
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀Feurking⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀"""
 
+def main(
+        description: str,
+        args_description: str,
+        parser: argparse.ArgumentParser
+    ) -> argparse.Namespace:
+
+    print(description, " - Version:", __version__)
+    
+    parser.add_argument("--file_path", type=str, required=True, help="Chemin vers le fichier CSV")
+    parser.add_argument("--category_threshold", type=int, default=10, help="Nombre max de catégories pour les variables ordinales")
+    parser.add_argument("--limit", type=int, default=None, help="Nombre maximum de lignes à charger")
+
+    parser.add_argument("--sample_size", type=float, default=0.1, help="Proportion de l'échantillon à sélectionner")
+    parser.add_argument("--random_state", type=int, default=42, help="Valeur pour initialiser la graine aléatoire")
+    parser.add_argument("--plot_type", type=str, default="numeric", help="Type de graphique à générer")
+    parser.add_argument("--colunm_name", type=str, default="cities", help="Nom de la colonne à visualiser")
+    
+    return parser.parse_args()
+
+def pars_args_data_frame_filter(args=None) -> None:
+
+    parser = argparse.ArgumentParser(description="Commandes relatives au DataFrameSelector script")
+
+    args = main("Bienvenue dans le DataFrameFilter", "Commandes relatives au DataFrameSelector script", parser)
+
+    selector = DataFrameSelector(args.limit, args.file_path)
+
+    selector.filter_columns_by_category_count(args.category_threshold)
+    selector.filter_columns_by_correlation(min_corr=0.1)
+
+    sample_df = selector.get_representative_sample(sample_size=args.sample_size, random_state=args.random_state)
+
+    selector.save_plot(args.colunm_name, args.plot_type, sample_df, "undersample_plot")
+
 def pars_args_data_frame_processor(args=None) -> None:
     
     print(pandas_display())
-    
+
     parser = argparse.ArgumentParser(description="Commandes relatives au DataFrameProcessor script")
     
-    parser.add_argument("--file_path", type=str, required=True, help="Chemin vers le fichier CSV, dans mon context data/en.openfoodfacts.org.products.csv")
-    parser.add_argument("--category_threshold", type=int, default=10, help="Nombre max de catégories pour les variables ordinales")
-    parser.add_argument("--limit", type=int, default=None, help="Nombre maximum de lignes à loader")
-    
-    args = parser.parse_args(args)
+    args = main("Bienvenue dans le DataFrameProcessor", "Commandes relatives au DataFrameProcessor script", parser)
 
     processor = DataFrameProcessor(args.file_path, args.category_threshold, args.limit)
     asyncio.run(processor.process_dataframe())
--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,6 +1,7 @@
-from . import pars_args_data_frame_processor
+from . import pars_args_data_frame_processor, pars_args_data_frame_filter
 
 import sys
 
 if __name__ == "__main__":
     pars_args_data_frame_processor(sys.argv[1:])
+    pars_args_data_frame_filter(sys.argv[1:])
