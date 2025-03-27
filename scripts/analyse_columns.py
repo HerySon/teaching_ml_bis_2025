367856e7c9403aedd37f1b203e28444cf1a60cea
MateoPerrotNasi <bew1432@gmail.com>
2025-03-27 11:12:33 +0100

    feat(analyse-columns): change accordingly to pr review

--- scripts/analyse_columns.py
+++ scripts/analyse_columns.py
@@ -1,10 +1,10 @@
 """Module providing a function to analyse and classify the columns of a DataFrame"""
 
 import pandas as pd
+import numpy as np
 
 
-def analyse_columns(df: pd.DataFrame, category_threshold: int = 10) \
-        -> [dict, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def analyse_columns(df: pd.DataFrame, ordinal_categories: []) -> dict:
     """
     Analyse a DataFrame and classify its columns into three categories:
     - Numeric
@@ -22,27 +22,29 @@ def analyse_columns(df: pd.DataFrame, category_threshold: int = 10) \
 
     # Dictionnaire to store the columns
     filtered_columns = {
-        "numeric": [],
-        "ordinal_categorical": [],
-        "non_ordinal_categorical": []
+        "numerical": {},
+        "ordinal_categorical": {},
+        "non_ordinal_categorical": {}
     }
 
-    for col in df.columns:
-        if df[col].dtype in ["int64", "float64"]:
-            filtered_columns["numeric"].append(col)
-        elif df[col].dtype == "object" or df[col].dtype.name == "category":
-            nb_categories = df[col].nunique()
-            if nb_categories <= category_threshold:
-                filtered_columns["ordinal_categorical"].append(col)
-            else:
-                filtered_columns["non_ordinal_categorical"].append(col)
+    for numerical_column in df.select_dtypes(include=["int64", "float64"]).columns:
+        filtered_columns["numerical"][numerical_column] = df[numerical_column].nunique()
+        col_type = df[numerical_column].dtype
 
-        # Downcasting des variables numériques
-        df.loc[:, filtered_columns["numeric"]] = df[filtered_columns["numeric"]].apply(
-            pd.to_numeric, downcast="float")
+        if np.issubdtype(col_type, np.integer):
+            for dtype in ["int8", "int16", "int32"]:
+                if df[numerical_column].min() >= np.iinfo(dtype).min:
+                    if df[numerical_column].max() <= np.iinfo(dtype).max:
+                        df.loc[:, numerical_column] = df[numerical_column].astype(dtype)
 
-    return (
-        filtered_columns,
-        df[filtered_columns["numeric"]],
-        df[filtered_columns["ordinal_categorical"]],
-        df[filtered_columns["non_ordinal_categorical"]])
+        elif np.issubdtype(col_type, np.floating):
+            if np.allclose(df[numerical_column], df[numerical_column].astype(np.float32)):
+                df.loc[:, numerical_column] = df[numerical_column].astype(np.float32)
+
+    for columns in df.select_dtypes(include=["object"]).columns:
+        if columns in ordinal_categories:
+            filtered_columns["ordinal_categorical"][columns] = df[columns].nunique()
+        else:
+            filtered_columns["non_ordinal_categorical"][columns] = df[columns].nunique()
+
+    return filtered_columns
